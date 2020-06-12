60e96b17cff1cd2e60fb47ab9b267ba03cf588fc
socamporomani <43774853+socamporomani@users.noreply.github.com>
2020-06-12 23:43:20 +0200

    Add files via upload

---
+++ Kinect2/Kinect2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Kinect2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStream = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStream
+            // 
+            this.btnStream.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStream.Location = new System.Drawing.Point(464, 355);
+            this.btnStream.Name = "btnStream";
+            this.btnStream.Size = new System.Drawing.Size(66, 29);
+            this.btnStream.TabIndex = 0;
+            this.btnStream.Text = "Play";
+            this.btnStream.UseVisualStyleBackColor = true;
+            this.btnStream.Click += new System.EventHandler(this.btnStream_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Estado";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ld";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(89, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(10, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "-";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(89, 40);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(31, 13);
+            this.lblId.TabIndex = 4;
+            this.lblId.Text = "no-Id";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(20, 74);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(510, 270);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 396);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStream);
+            this.Name = "Form1";
+            this.Text = "Manzar";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStream;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Kinect2/Kinect2/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+namespace Kinect2
+{
+    public partial class Form1 : Form
+    {
+        private KinectSensor kSensor;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /*enceder la kinect al pulsar el boton*/
+        private void btnStream_Click(object sender, EventArgs e)
+        {
+            if(btnStream.Text == "Play")
+            {
+              
+                if(KinectSensor.KinectSensors.Count>0)
+                {
+                    this.btnStream.Text = "Stop";
+                    kSensor = KinectSensor.KinectSensors[0];
+                    KinectSensor.KinectSensors.StatusChanged += KinectSensors_StatusChanged;
+                }
+                kSensor.Start();
+                /* muestra el estado del kinect por uno valores predefinidos*/
+                this.lblId.Text = kSensor.DeviceConnectionId;
+                kSensor.ColorStream.Enable(ColorImageFormat.RgbResolution1280x960Fps12);
+                kSensor.ColorFrameReady += KSensor_ColorFrameReady;
+            }
+            else
+            {
+
+                if(kSensor !=null && kSensor.IsRunning)
+                {
+                    kSensor.Stop();
+                    this.btnStream.Text = "Play";
+                    this.pictureBox.Image = null;
+                }
+            }
+        }
+/*lo elimina de la memoria*/
+        private void KSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            
+            using (var frame = e.OpenColorImageFrame())
+            {
+                /*crea imagen*/
+            }
+        }
+
+        private void KinectSensors_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            this.lblStatus.Text = kSensor.Status.ToString();
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+        private Bitmap CreateBitmatFromSensor(ColorImageFrame frame)
+        {
+            var pixelData = new byte[frame.PixelDataLength];
+            frame.CopyPixelDataTo(pixelData);
+            var stride = frame.Width * frame.BytesPerPixel;
+
+            var bmpFrame = new Bitmap(frame.Width, frame.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            var bmpData = bmpFrame.LockBits(new Rectangle(0, 0, frame.Width, frame.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, bmpFrame.PixelFormat);
+
+            System.Runtime.InteropServices.Marshal.Copy(pixelData, 0, bmpData.Scan0, frame.PixelDataLength);
+
+            bmpFrame.UnlockBits(bmpData);
+        }
+    }
+}
